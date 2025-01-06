8ce3f87131d25ad06f591a2c274864453ffcc102
srivastavavivek515 <srivastavavivek515@gmail.com>
2025-01-07 02:29:59 +0530

    Change in master branch 2

--- app/src/main/java/com/example/githubdemo2/MainActivity.kt
+++ app/src/main/java/com/example/githubdemo2/MainActivity.kt
@@ -12,5 +12,6 @@ class MainActivity : AppCompatActivity() {
         Log.d("Test","This is commit2")
         Log.d("Test","This is commit3")
         Log.d("Test","Change in master branch 1")
+        Log.d("Test","Change in master branch 2")
     }
 }
