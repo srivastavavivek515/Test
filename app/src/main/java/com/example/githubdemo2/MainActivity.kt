e9339706158a9554d5691a55898eeb48358eef9d
srivastavavivek515 <srivastavavivek515@gmail.com>
2025-01-07 02:20:59 +0530

    This is commit2

--- app/src/main/java/com/example/githubdemo2/MainActivity.kt
+++ app/src/main/java/com/example/githubdemo2/MainActivity.kt
@@ -9,5 +9,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.d("Test","This is commit1")
+        Log.d("Test","This is commit2")
     }
 }
